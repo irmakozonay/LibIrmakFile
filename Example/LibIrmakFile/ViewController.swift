af0b8a1faaac0eaa05ced916f90ad134c8b9a209
irmakozonay <irmakoz@msn.com>
2021-03-19 15:45:59 +0300

    zip

--- Example/LibIrmakFile/ViewController.swift
+++ Example/LibIrmakFile/ViewController.swift
@@ -15,7 +15,6 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        let logger = Logger()
         logger.printLog()
         // Do any additional setup after loading the view, typically from a nib.
     }
